c6bbb5b00601e5ea3430e06dd98f5e89da38897e
anna <anna@rcbd.org>
2022-12-20 23:08:37 -0500

    Implement downloading questions data from https://opentdb.com/api_config.php to JSON files. Modify the seed to get data from the same json files.

---
+++ seed/fetch.js
@@ -0,0 +1,44 @@
+//Create object axios
+import axios from "axios";
+//fs - file system
+import fs from "fs";
+
+let categoryID = [18, 19, 20];
+
+categoryID.forEach((element) => {
+  axios
+    .get("https://opentdb.com/api.php", {
+      params: {
+        amount: 10,
+        category: element,
+      },
+    })
+    .then(function (response) {
+      // handle success
+      // console.log(response.data.results);
+      let data = response.data.results.map((item) => {
+        let answers = item.incorrect_answers;
+        answers.push(item.correct_answer);
+        return {
+          category: item.category,
+          title: item.question,
+          correct_answer: item.correct_answer,
+          answers: answers,
+        };
+      });
+      console.log(data);
+      let jsonData = JSON.stringify(data);
+      fs.writeFile(`questions-${element}.json`, jsonData, function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+});
--- seed/seed.js
+++ seed/seed.js
@@ -1,14 +1,21 @@
 import db from "../db/connection.js";
 import Category from "../models/category.js";
 import Question from "../models/question.js";
+
 import categories from "./categories.json" assert { type: "json" };
 import questions from "./questions.json" assert { type: "json" };
+import questions_18 from "./questions-18.json" assert { type: "json" };
+import questions_19 from "./questions-19.json" assert { type: "json" };
+import questions_20 from "./questions-20.json" assert { type: "json" };
 
 const insertData = async () => {
   await db.dropDatabase();
 
   await Category.create(categories);
   await Question.create(questions);
+  await Question.create(questions_18);
+  await Question.create(questions_19);
+  await Question.create(questions_20);
 
   await db.close();
 };
